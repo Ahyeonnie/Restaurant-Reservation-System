1f20e08ec1998bde40562164d343942b90f99e8c
Ahyeonnie <rjmanongsong.personal@gmail.com>
2026-06-15 23:22:50 +0800

    Menu Dashboard

---
+++ frontend/src/pages/Dashboard/Menu_Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Menu_dashboard (){
+    return(
+        <div className="new">
+            <h1>Menu Dashboard</h1>
+            <p>Welcome to the Menu Dashboard!</p>
+        </div>
+    );
+}
