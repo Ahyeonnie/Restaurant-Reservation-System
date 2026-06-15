1ae8325c18ae5dd7bdbfa5182690aeeb35374606
Ahyeonnie <rjmanongsong.personal@gmail.com>
2026-06-15 23:21:05 +0800

    Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -27,15 +27,14 @@ export default function Navbar() {
         <p className='text-xl font-bold text-white'>
           QuickBite
         </p>
-        <ul className= {`nav-links ${ isOpen ? "flex flex-col space-y-4 transition-all duration-300" : "hidden"} md:flex md:space-x-6`}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/menu">Menu</Link></li>
-          <li><Link to="/news">News</Link></li>
-          <li><Link to="/reservations">Reservations</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-
+          <ul className= {`nav-links ${ isOpen ? "flex flex-col space-y-4 transition-all duration-300" : "hidden"} md:flex md:space-x-6`}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/news">News</Link></li>
+            <li><Link to="/reservations">Reservations</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
         <button className="md:hidden p-2 rounded focus:outline-none focus:ring-2 focus:ring-ring" onClick = {toggleMenu}>
           <img 
             src={hamburgerIcon} 
